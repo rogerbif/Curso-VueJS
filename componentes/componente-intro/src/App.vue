fd32eb77239412cc0edbe69c3c2d2477cd66056e
Roger <roger@interprocess.com.br>
2021-02-04 22:39:26 -0300

    Componentes

--- componentes/componente-intro/src/App.vue
+++ componentes/componente-intro/src/App.vue
@@ -1,18 +1,13 @@
 <template>
   <div id="app">
-    <h1>Contadores</h1>
-    <app-contador></app-contador>
-    <app-contador></app-contador>
-    <app-contador></app-contador>
+    <app-contadores></app-contadores>
+    <button>?</button>
   </div>
 </template>
 
 <script>
-export default {
-  
-}
+export default {};
 </script>
 
 <style>
-
 </style>
--- componentes/componente-intro/src/Contador.vue
+++ componentes/componente-intro/src/Contador.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="contador">
     <span>{{ contador }}</span>
-    <button @click="adicionar">+</button> 
-    <button @click="subtrair">-</button> 
+    <button @click="adicionar">+</button>
+    <button @click="subtrair">-</button>
   </div>
 </template>
 
@@ -10,35 +10,34 @@
 export default {
   data() {
     return {
-      contador: 0      
-    }
+      contador: 0,
+    };
   },
   methods: {
-      adicionar(){
-          this.contador++
-      },
-      subtrair(){
-          this.contador--
-      }
+    adicionar() {
+      this.contador++;
+    },
+    subtrair() {
+      this.contador--;
+    },
   },
-}
+};
 </script>
 
-<style>
-    .contador span {
-        border-bottom: 1px solid #ccc;
-        height: 30px;
-        padding: 5px 25px;
-    }
+<style scoped>
+span {
+  border-bottom: 1px solid #ccc;
+  height: 30px;
+  padding: 5px 25px;
+}
 
-    .contador button {
-        height: 30px;
-        width: 30px;
-        border-radius: 15px;
-        background-color: coral;
-        color: #fff;
-        margin-left: 10px;
-        outline: none;
-    }
-
+button {
+  height: 30px;
+  width: 30px;
+  border-radius: 15px;
+  background-color: coral;
+  color: #fff;
+  margin-left: 10px;
+  outline: none;
+}
 </style>
