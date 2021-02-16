56648499497070c8c130d2a627245c657d9b868c
Roger <roger@interprocess.com.br>
2021-02-16 18:44:28 -0300

    .

---
+++ componentes/desafio3/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app>
+
+    <app-menu />
+
+    <app-toolbar />
+
+    <v-content>
+      <app-carousel />
+    </v-content>
+
+    <app-footer />
+  </v-app>
+</template>
+
+<script>
+import Footer from './components/template/Footer.vue'
+import Carousel from './components/widgets/Carousel.vue'
+import Toolbar from './components/template/Toolbar.vue'
+import Menu from './components/template/Menu.vue'
+
+export default {
+  components: {'app-footer': Footer, 
+  'app-carousel': Carousel, 
+  'app-toolbar': Toolbar,
+  'app-menu': Menu },
+}
+</script>
