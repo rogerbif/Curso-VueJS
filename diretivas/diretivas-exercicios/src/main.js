2bc10837bb084f723d20309f60162b5d51520edd
Roger <roger@interprocess.com.br>
2021-02-25 20:41:01 -0300

    diretivas

--- diretivas/diretivas-exercicios/src/main.js
+++ diretivas/diretivas-exercicios/src/main.js
@@ -4,7 +4,19 @@ import App from './App.vue'
 Vue.config.productionTip = false
 
 Vue.directive('destaque',{
-	
+	bind(el, binding, vnode) {
+		///el.style.backgroundColor = 'Lightgreen'
+		let atraso = 0
+		if(binding.modifiers['atrasar']) atraso = 3000
+
+		setTimeout(()=> {
+			if(binding.arg === 'fundo'){
+				el.style.backgroundColor = binding.value
+			} else {
+				el.style.color = binding.value
+			}
+		}, atraso)
+	}	
 })
 
 new Vue({
