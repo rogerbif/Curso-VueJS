2aadc28d3323a1a542733836d30bcfcb7950e9c9
Roger <roger@interprocess.com.br>
2021-03-03 08:54:58 -0300

    Mixins

--- filtros-mixins/filtros-mixins-exercicios/src/App.vue
+++ filtros-mixins/filtros-mixins-exercicios/src/App.vue
@@ -2,20 +2,27 @@
 	<div id="app">
 		<h1>Filtros & Mixins</h1>
 		<hr>
+		<p>{{ usuarioLogado }}</p>
 		<p>{{ cpfDoAluno | cpf | inverter }}</p>
 		<input type="text" :value="cpfDoAluno | cpf">
 		<hr>
 		<Frutas />
+		<hr>
+		<ul>
+            <li v-for="fruta in frutas" :key="fruta">{{ fruta }}</li>
+        </ul>
+        <input type="text" v-model="fruta" @keydown.enter="add">
 	</div>
 </template>
 
 <script>
 import Frutas from './Frutas.vue'
 import frutasMixin from './frutasMixin'
+import usuarioMixin from './usuarioMixin'
 
 export default {
 	components: { Frutas },
-	mixins: [ frutasMixin ],
+	mixins: [ frutasMixin, usuarioMixin ],
 	filters: {
 		cpf(valor){
 			const arr = `${valor}`.split('') //`${valor}` transforma o valor para string
