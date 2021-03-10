81fb8b42e40a4db19f1a3dd7844ea1cb1cf9eb2c
Roger <roger@interprocess.com.br>
2021-03-09 22:49:57 -0300

    .

---
+++ rotas/rotas-exercicios/src/components/template/Menu.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="menu">
+        <ul>
+            <router-link to="/" tag="li" active-class="active" exact>
+            <a>Início</a></router-link>        
+            <router-link to="/usuario/1" tag="li" active-class="active">
+            <a>Usuário 1</a></router-link>   
+            <router-link to="/usuario/2" tag="li" active-class="active">
+            <a>Usuário 2</a></router-link>   
+        </ul>     
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+
+</style>
