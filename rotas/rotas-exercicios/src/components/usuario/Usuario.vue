1e5c138db6d13b1d9ec95a410610aa99a24cfc8a
Roger <roger@interprocess.com.br>
2021-03-10 08:14:34 -0300

    .

--- rotas/rotas-exercicios/src/components/usuario/Usuario.vue
+++ rotas/rotas-exercicios/src/components/usuario/Usuario.vue
@@ -2,31 +2,20 @@
     <div class="usuario">
         <h2>Usuários</h2>    
         <hr>
-        <p><strong>Código: </strong>{{ id }}</p>
+        <router-view/>
         <button sucesso @click="irParaInicio">Voltar</button>
     </div>
 </template>
 
 <script>
 export default {
-    props: ['id'],
-    // data() {
-    //     return {
-    //         id: this.$route.params.id
-    //     }
-    // },
-    // watch: {
-    //     $route(to, from) {
-    //         this.id = to.params.id
-    //     }
-    // },
     methods: {
         irParaInicio(){
             //this.$router.push('/')
-            this.$router.push({ path: '/' })
+            //this.$router.push({ path: '/' })
+            this.$router.push({ name: 'inicio' })
         }
     },
-    
 }
 </script>
 
